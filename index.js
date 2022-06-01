3f269e7392059911837dc758d091078a3b9ca61c
Thaina Maria Camarelli da Fonseca <thaina.fonseca@SP-MBZ3748.local>
2022-06-01 19:45:45 -0300

    alter JS

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-var choises = document.getElementsByClassName("rating-numbers")
+let choises = document.getElementsByClassName("rating-numbers")
 
 document.querySelector(".thankyou-div").style.display = "none"
 
 for (let i = 0; i <choises.length; i++){
     choises[i].addEventListener("click",function(){
-        var chosenNumber = choises[i].textContent
-        document.querySelector(".thanks-choice").innerHTML = chosenNumber
-        choises[i].classList.add("rating-numbers-active")
-        choises[i].classList.add("rating-numbers-active:hover")
+    validateColor()
+
+    choises[i].classList.toggle("rating-numbers-active")
+    let chosenNumber = choises[i].textContent 
+    document.querySelector(".thanks-choice").innerHTML = chosenNumber  
+     
     })
     
 }
 
-function changeDivs(){
-    document.querySelector(".rating-div").style.display = "none"
-    document.querySelector(".thankyou-div").style.display = "block"
+function validateColor(){
+    for(let i =0; i <choises.length; i++){
+        choises[i].classList.remove("rating-numbers-active") 
+    }
 }
 
-document.querySelector("a").addEventListener("click",changeDivs);
+document.querySelector("a").addEventListener("click",function(){
+    document.querySelector(".rating-div").style.display = "none"
+    document.querySelector(".thankyou-div").style.display = "block"
+});
 
 
 
